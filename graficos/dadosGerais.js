0aa8ceb309267d02011339fc7b720ad5771a25ae
AnnyC28 <160602515+AnnyC28@users.noreply.github.com>
2024-11-14 10:35:53 -0300

    14/11

--- graficos/dadosGerais.js
+++ graficos/dadosGerais.js
@@ -5,15 +5,32 @@ async function visualizaInfo() {
     const dados = await res.json()
 
     const musica = Object.keys(dados)
+    const votos = Object.values(dados)
     const musicaMaisVotada = musica[0]
-    const votos = Object.values(dados)[0]
+    const quantidadeDeVotos = Object.values(dados)[0]
 
+    const data = [
+        {
+            x:musica,
+            y:votos,
+            type: 'bar'
+        }
+    ]
+    
     let paragrafo = document.createElement('p')
     paragrafo.classList.add('caixa-grafico__texto')
-    paragrafo.innerHTML = `Nessa pesquisa, buscou se compreender qual a música favorita dos brasileiros que gostam de ouvir uma boa playlist em uma tarde de domingo. A ${musicaMaisVotada} foi a musica mais votada entre todas, com um total de ${votos} votos, em uma pesquisa que teve um total de um milhão de participantes`
+    paragrafo.innerHTML = `Nessa pesquisa, buscou se compreender qual a música favorita dos brasileiros que gostam de ouvir uma boa playlist em uma tarde de domingo. A ${musicaMaisVotada} foi a musica mais votada entre todas, com um total de ${quantidadeDeVotos} votos, em uma pesquisa que teve um total de um milhão de participantes`
 
     let caixa = document.getElementById('caixa-grafico')
     caixa.appendChild(paragrafo)
+
+    const grafico = document.createElement('div')
+    grafico.className = 'grafico'
+    document.getElementById('caixa-grafico').appendChild(grafico)
+    Plotly.newPlot(grafico, data)
+
+
+
 }
 
 visualizaInfo()
